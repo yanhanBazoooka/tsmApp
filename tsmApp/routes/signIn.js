7c6c4f9baab032ee020eb240f77d72e8a64eac13
Han Yan <yh20020606@gmail.com>
2024-04-01 15:56:59 -0400

    first commit

---
+++ tsmApp/routes/signIn.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET sign-in page. */
+router.post('/signIn', function(req, res, next) {
+    //TODO: add validation for email and password
+    validateResult = true;
+    if (validateResult == true) {
+        //go to dashboard page
+        res.render('dashBoard', { title: 'Welcome to your dashboard' });
+    }
+    else {    // incorrect credentials
+        //stay in the home page/same page
+        res.render('index', { title: 'Express' });
+    }
+});
+
+module.exports = router;
